84572337a385c2d4cb537eb2b3c8809d3bd75c0f
George Legeza <glegeza@gmail.com>
2018-08-25 20:51:48 -0400

    Finish maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let aggr = {};
+    let max = 0;
+    let maxChar = '';
+    for (let c of str) {
+        aggr[c] = aggr[c] + 1 || 1;
+        if (aggr[c] > max) {
+            max = aggr[c];
+            maxChar = c;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
