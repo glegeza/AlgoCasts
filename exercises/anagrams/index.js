65458415a13a1677ba45c5f4b3a430526f188272
George Legeza <glegeza@gmail.com>
2018-08-28 15:04:30 -0400

    Finish first pass at anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function isAlpha(cc) {
+    return ((cc>47 && cc<58) || (cc>64 && cc<91) || (cc>96 && cc<123));
+}
+
+function anagrams(stringA, stringB) {
+    const map = {};
+    let lowerA = stringA.replace(/[^\w]/g, "").toLowerCase();
+    let lowerB = stringB.replace(/[^\w]/g, "").toLowerCase();
+    if (lowerA.length !== lowerB.length) {
+        return false;
+    }
+
+    for (let c of lowerA) {
+        if (!map[c]) {
+            map[c] = [0, 0];
+        }
+        map[c][0] += 1;
+    }
+    for (let c of lowerB) {
+        if (!map[c]) {
+            map[c] = [0, 0];
+        }
+        map[c][1] += 1;
+    }
+
+    console.log(map);
+
+    for (let c in map) {
+        if (map[c][0] !== map[c][1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 module.exports = anagrams;
