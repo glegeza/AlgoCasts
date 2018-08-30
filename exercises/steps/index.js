f331d80c3fcc50c644dc1b800dbad4379ba4f59a
George Legeza <glegeza@gmail.com>
2018-08-30 15:49:15 -0400

    Finish steps exercise with recursive solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,28 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//     for (let i = 1; i <= n; i++) {
+//         console.log(Array(i + 1).join('#') + Array(n + 1 - i).join(' '));
+//     }
+// }
+
+function steps(n, row=0) {
+    if (row > n) {
+        return;
+    }
+    console.log(Array(row + 1).join('#') + Array(n - row + 1).join(' '));
+    steps(n, row + 1)
+}
+
+// function steps(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let str = '';
+//         for (let j = 1; j <= n; j++) {
+//             str += j <= i ? '#' : ' ';
+//         }
+//         console.log(str);
+//     }
+// }
 
 module.exports = steps;
